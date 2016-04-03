480a2a7de25d1500ae8a06311518c2317094be75
Bruno Rino <bruno.rino@gmail.com>
2016-04-03 22:39:45 +0200

    refactoring

--- django_ext/templatetags/getattribute.py
+++ django_ext/templatetags/getattribute.py
@@ -7,15 +7,15 @@ register = template.Library()
 @register.filter()
 def getattribute(value, arg):
     """Gets an attribute of an object dynamically from a string name"""
-
     if hasattr(value, 'keys') and arg in value:
-        return value[arg]
+        result = value[arg]
     elif hasattr(value, str(arg)):
         result = getattr(value, arg)
         if callable(result):
             result = result()
-        return result
+        result = result
     elif arg.isdigit() and len(value) > int(arg):
-        return value[int(arg)]
+        result = value[int(arg)]
     else:
-        return settings.TEMPLATE_STRING_IF_INVALID
+        result = settings.TEMPLATE_STRING_IF_INVALID
+    return result
