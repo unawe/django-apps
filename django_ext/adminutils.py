c6336da9825f70f775ff3c72778c1d747d60c668
Bruno Rino <bruno.rino@gmail.com>
2017-03-04 19:01:49 +0100

    code cleanup

--- django_ext/adminutils.py
+++ django_ext/adminutils.py
@@ -1,16 +1,14 @@
-
 import csv
+
 
 # Python 3 version works natively with unicode!!!
 def download_csv(modeladmin, request, queryset, filename='export.csv'):
-    import csv
     from django.http import HttpResponse
     from django.core.exceptions import PermissionDenied
 
     if not request.user.is_staff:
         raise PermissionDenied
     opts = queryset.model._meta
-    model = queryset.model
     response = HttpResponse(content_type='text/csv')
     # force download.
     response['Content-Disposition'] = 'attachment;filename=%s' % filename
@@ -24,4 +22,3 @@ def download_csv(modeladmin, request, queryset, filename='export.csv'):
         writer.writerow([getattr(obj, field) for field in field_names])
     return response
 download_csv.short_description = "Download selected as csv"
-
