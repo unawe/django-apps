f19e629271fe5a7ea46d45f7354808ce70a54106
Bruno Rino <bruno.rino@gmail.com>
2017-03-04 19:02:45 +0100

    re-activate filemanager admin

--- filemanager/admin.py
+++ filemanager/admin.py
@@ -2,28 +2,28 @@ from django.contrib import admin
 from django import forms
 from django.core import validators
 
-# from filemanager.models import File, Folder
-
-# class FolderAdminForm(forms.ModelForm):
-    
-#     class Meta:
-#         model = Folder
-#         fields = ('title', )
-
-#     def clean(self):
-#         cleaned_data = super().clean()
-#         # cleaned_data['title'] = cleaned_data['title'].lowercase
-#         return cleaned_data
+from filemanager.models import File, Folder
 
 
-# class FolderAdmin(admin.ModelAdmin):
-# 	form = FolderAdminForm
+class FolderAdminForm(forms.ModelForm):
+    class Meta:
+        model = Folder
+        fields = ('title', )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        # cleaned_data['title'] = cleaned_data['title'].lowercase
+        return cleaned_data
 
 
-# class FileAdmin(admin.ModelAdmin):
-#     list_display = ('title', 'folder', )
-#     ordering = ('folder', 'title', )
+class FolderAdmin(admin.ModelAdmin):
+    form = FolderAdminForm
 
 
-# admin.site.register(File, FileAdmin)
-# admin.site.register(Folder, FolderAdmin)
+class FileAdmin(admin.ModelAdmin):
+    list_display = ('title', 'folder', )
+    ordering = ('folder', 'title', )
+
+
+admin.site.register(File, FileAdmin)
+admin.site.register(Folder, FolderAdmin)
