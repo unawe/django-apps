2cafb8a6b8f37d95ac5cbc7984acf8c1227ae972
Bruno Rino <bruno.rino@gmail.com>
2017-03-06 15:37:32 +0100

    re-enabled command line generation of download formats (for activities, spacescoops)

--- activities/management/commands/compile_spacescoop.py
+++ activities/management/commands/compile_spacescoop.py
@@ -5,14 +5,14 @@ from django_ext.compiler import PublishingBaseCommand
 
 class Command(PublishingBaseCommand):
 
-    help = 'Generate downloads for an activity'
+    help = 'Generate downloads for a Space Scoop article'
 
     def __init__(self, *args, **kwargs):
-        self.objdef = settings.ACTIVITY_DOWNLOADS
+        self.objdef = settings.SPACESCOOP_DOWNLOADS
         super().__init__(*args, **kwargs)
 
     def add_arguments(self, parser):
         # Positional arguments
-        parser.add_argument('code', help='Four digit code (YYnn) of the activity')
-        parser.add_argument('lang', help='Language of the activity')
+        parser.add_argument('code', help='Four digit code (YYnn) of the article')
+        parser.add_argument('lang', help='Language of the article')
         super().add_arguments(parser)
--- django_ext/compiler.py
+++ django_ext/compiler.py
@@ -5,6 +5,7 @@ import importlib
 # from django.template import loader, Context, Template
 from django.conf import settings
 from django.utils.translation import activate
+from django.core.management.base import BaseCommand
 
 # import the logging library
 import logging
@@ -19,71 +20,108 @@ def get_python_thing(fullname):
     return getattr(module, thing_name)
 
 
+def generate_one(objdef, obj, file_type, force=False, site_url=None):
+    ctx = {'slug': obj.slug, 'code': obj.code, 'ext': file_type}
+    if hasattr(obj, 'language_code'):
+        ctx['lang'] = obj.language_code
+    filename = objdef['filename_tpl'] % ctx
+    path = os.path.join(settings.MEDIA_ROOT, objdef['path'], filename)
+    if force or (not (os.path.exists(path) and os.path.getmtime(path) > time.mktime(obj.modification_date.timetuple()))):
+        if not site_url:
+            site_url = settings.SITE_URL
+        renderer = get_python_thing(objdef['renderers'][file_type])
+        renderer(obj, path, site_url=site_url)
+    return filename
+
+
 def get_generated_url(objdef, file_type, code, lang=None):
     model = get_python_thing(objdef['model'])
     if lang:
         obj = model.objects.available().language(lang).get(code=code)
         activate(lang)  # required if called from command line ??? TODO: check
-        filename = objdef['filename_tpl'] % {'slug': obj.slug, 'code': obj.code, 'lang': obj.language_code, 'ext': file_type}
     else:
         obj = model.objects.available().get(code=code)
-        filename = objdef['filename_tpl'] % {'slug': obj.slug, 'code': obj.code, 'ext': file_type}
-    path = os.path.join(settings.MEDIA_ROOT, objdef['path'], filename)
-    if not (os.path.exists(path) and os.path.getmtime(path) > time.mktime(obj.modification_date.timetuple())):
-        renderer = get_python_thing(objdef['renderers'][file_type])
-        renderer(obj, path, site_url=settings.SITE_URL)
+    filename = generate_one(objdef, obj, file_type)
     return settings.MEDIA_URL + objdef['path'] + filename
 
 
-# LANGUAGES = [code for (code, name) in settings.LANGUAGES]
+LANGUAGES = [code for (code, name) in settings.LANGUAGES]
 
-# class PdfCompiler(object):
 
-#     def __init__(self, clazz, out_path, out_url, pdf_filename, print_preview_urlpath):
-#         self.clazz = clazz
-#         self.out_path = out_path
-#         self.out_url = out_url
-#         self.pdf_filename = pdf_filename
-#         self.print_preview_urlpath = print_preview_urlpath
+def _generate_formats(objdef, obj, formats, force, site_url):
+    for fmt in formats:
+        generate_one(objdef, obj, fmt, force=force, site_url=site_url)
 
-#     def make_pdf(self, code, lang, site_url):
-#         if code == 'all':
-#             if lang == 'all':
-#                 for baseobj in self.clazz.objects.available():
-#                     for lang in baseobj.get_available_languages():
-#                         if lang in LANGUAGES:
-#                             obj = self.clazz.objects.available().language(lang).get(code=baseobj.code)
-#                             self.generate_pdf(obj, site_url)
-#             else:
-#                 for obj in self.clazz.objects.available().language(lang):
-#                     self.generate_pdf(obj, site_url)
-#         else:
-#             if lang == 'all':
-#                 baseobj = self.clazz.objects.available().get(code=code)
-#                 for lang in baseobj.get_available_languages():
-#                     if lang in LANGUAGES:
-#                         obj = self.clazz.objects.available().language(lang).get(code=code)
-#                         self.generate_pdf(obj, site_url)
-#             else:
-#                 obj = self.clazz.objects.available().language(lang).get(code=code)
-#                 self.generate_pdf(obj, site_url)
 
-#     def get_pdf(self, code, lang, pdf_type):
-#         """
-#         :param code: id of text to generate
-#         :param lang: language of PDF
-#         :param pdf_type: type of pdf to generate. every type can has own templates.
-#         :return: path to PDF
-#         """
+def generate(objdef, options):
+    model = get_python_thing(objdef['model'])
+    code = options['code']
+    lang = options['lang']
+    force = options['force']
+    site_url = options['site_url']
 
-#         obj = self.clazz.objects.available().language(lang).get(code=code)
-#         filename = self.pdf_filename(obj)
-#         path = os.path.join(self.out_path, filename)
-#         if not (os.path.exists(path) and os.path.getmtime(path) > time.mktime(obj.modification_date.timetuple())):
-#             if pdf_type == 'activities':
-#                 self.generate_activities_pdf(obj, settings.SITE_URL)
-#             elif pdf_type == 'scoops':
-#                 self.generate_scoops_pdf(obj, settings.SITE_URL)
-#             else:
-#                 raise Exception('type of PDF document is not set')
-#         return os.path.join(self.out_url, filename)
+    formats = [fmt for fmt in settings.ACTIVITY_DOWNLOADS['renderers'].keys() if options[fmt]]
+
+    if code == 'all':
+        if lang == 'all':
+            for baseobj in model.objects.available():
+                for lang in baseobj.get_available_languages():
+                    if lang in LANGUAGES:
+                        obj = model.objects.available().language(lang).get(code=baseobj.code)
+                        _generate_formats(objdef, obj, formats, force, site_url)
+        else:
+            for obj in model.objects.available().language(lang):
+                _generate_formats(objdef, obj, formats, force, site_url)
+
+    else:
+        if lang == 'all':
+            baseobj = model.objects.available().get(code=code)
+            for lang in baseobj.get_available_languages():
+                if lang in LANGUAGES:
+                    obj = model.objects.available().language(lang).get(code=code)
+                    _generate_formats(objdef, obj, formats, force, site_url)
+
+        else:
+            obj = model.objects.available().language(lang).get(code=code)
+            _generate_formats(objdef, obj, formats, force, site_url)
+
+
+class PublishingBaseCommand(BaseCommand):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.formats = self.objdef['renderers'].keys()
+
+    def add_arguments(self, parser):
+        # Named (optional) arguments
+        for fmt in self.formats:
+            parser.add_argument(
+                '--%s' % fmt,
+                action='store_true',
+                dest=fmt,
+                default=False,
+                help='Generate only %s file' % fmt)
+        parser.add_argument(
+            '--force',
+            action='store_true',
+            dest='force',
+            default=None,
+            help='Force generation of files, even if existing files are up-to-date')
+        parser.add_argument(
+            '--site-url',
+            action='store',
+            dest='site_url',
+            default=None,
+            help='Connect to a website other than %s' % settings.SITE_URL)
+
+    def handle(self, *args, **options):
+        # if no format was selected, select them all
+        if not any([options[fmt] for fmt in self.formats]):
+            for fmt in self.formats:
+                options[fmt] = True
+        generate(self.objdef, options)
+
+    def _generate_downloads(self, activity, options):
+        for fmt in self.formats:
+            if options['all'] or options[fmt]:
+                generate_one(self.objdef, fmt, activity, force=options['force'], site_url=options['site_url'])
