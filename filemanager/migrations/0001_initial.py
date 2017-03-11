84d7814b5209415ab45ca592907d37ca7a0ab809
Bruno Rino <bruno.rino@gmail.com>
2017-03-11 17:08:22 +0100

    python3 fixes

--- filemanager/migrations/0001_initial.py
+++ filemanager/migrations/0001_initial.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
             name='File',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('title', models.CharField(help_text=b'Leave blank to use filename', max_length=255, blank=True)),
+                ('title', models.CharField(help_text='Leave blank to use filename', max_length=255, blank=True)),
                 ('file', models.FileField(upload_to=filemanager.models.upload_to)),
             ],
             options={
@@ -28,7 +28,7 @@ class Migration(migrations.Migration):
             name='Folder',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('title', models.CharField(help_text=b'Please use only alphabetic characters, numbers, and "-", "_" and "/"', unique=True, max_length=255, validators=[django.core.validators.RegexValidator(regex=b'^[-a-zA-Z0-9_/]+$')])),
+                ('title', models.CharField(help_text='Please use only alphabetic characters, numbers, and "-", "_" and "/"', unique=True, max_length=255, validators=[django.core.validators.RegexValidator(regex=r'^[-a-zA-Z0-9_/]+$')])),
             ],
             options={
                 'ordering': ['title'],
